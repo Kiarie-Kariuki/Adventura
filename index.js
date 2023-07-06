3ea52f39dbfa645081be808aa4986da564b55fb6
Collins <collinskiarie13@outlook.com>
2023-07-06 17:27:33 +0300

    Added a slider function for the hotel cards and changed the div names for the different cards

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function getTourPackages(){
 
                     card.innerHTML=`
                     <h3>${obj.name}</h3>
-                    <img src="${obj.image}" alt = 'image'></img>
+                    <img id='tourimg'src="${obj.image}" alt = 'image'></img>
                     <p>${obj.description}</p>
                     <p>Activities: ${obj.activities}</p>
                     `;
@@ -38,6 +38,8 @@ function getTourPackages(){
         })
 }
 
+let currentPosition = 0;
+const slideAmount = 30; //slide amount of the cards
 
 //fetching the hotels
 function getHotels(){
@@ -50,21 +52,42 @@ function getHotels(){
                
                 // Create cards
                const hotelCardsContainer = document.getElementById('hotelCardsContainer');
-
+               
                const card = document.createElement('div');
-                   card.className = 'card';
+                   card.className = 'cardHotel';
 
                    card.innerHTML=`
                    <h3>${obj.name}</h3>
-                   <img src="${obj.image}" alt = 'image'></img>
+                   <div class='pic'><img id='hotelimg 'src="${obj.image}" alt = 'image'></img></div>
                    <p>Location: ${obj.location}</p>
                    <p>Rating: ${obj.rating}</p>
                    <button id='btn'> Book A Room </button>
                    `;
                hotelCardsContainer.appendChild(card);
         }
+
+         // Apply sliding effect
+      const intervalTime = 2000; // sets the slide to take place after 2000ms
+      const slideInterval = setInterval(slideLeft, intervalTime);
+
+      function slideLeft() {
+        currentPosition -= slideAmount;
+        hotelCardsContainer.style.transition = 'transform 0.5s ease-in-out';
+        hotelCardsContainer.style.transform = `translateX(${currentPosition}px)`;
+        
+        // Reset position to start if reached the end
+        if (currentPosition <= -(slideAmount * (data.length - 1))) {
+          currentPosition = 0;
+          setTimeout(() => {
+            hotelCardsContainer.style.transition = 'none';
+            hotelCardsContainer.style.transform = `translateX(${currentPosition}px)`;
+          }, 100);
+        }
+      }
     })
 }
+
+
 
 // fetching client reviews of Adventura
 function getReviews(){
@@ -78,10 +101,10 @@ function getReviews(){
                const reviewCardsContainer = document.getElementById('review');
 
                const card = document.createElement('div');
-                   card.className = 'card';
+                   card.className = 'cardReview';
 
                    card.innerHTML=`
-                   <img src="${obj.image}" alt = 'image'></img>
+                   <img id='reviewImage'src="${obj.image}" alt = 'image'></img>
                    <h3>${obj.name}</h3>
                    <p>${obj.review}</p>
                    `;
